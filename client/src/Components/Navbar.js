02f80a1296359958e4c4dc132b1e4f5ce04cfab9
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-01-13 22:15:47 +0530

    ngo and restro

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { NavLink } from 'react-router-dom'
+import SigninN from './NGO/SigninN';
+import SigninR from './Restaurant/SigninR';
 
 const Navbar = () => {
+
+  const [SigninNOpen, setSigninNOpen] = useState(false)
+  const [SigninROpen, setSigninROpen] = useState(false)
   return (
     <div className='h-20 bg-slate-100 w-full flex justify-between mx-auto items-center'>
       <div className="nav-container ml-11 text-blue-500 font-bold text-lg">
@@ -10,8 +15,12 @@ const Navbar = () => {
 
       <div className="heading-container">
         <NavLink to={'/'} className="mr-14 text-blue-500 font-bold text-lg hover:text-blue-600">Home</NavLink>
-        <NavLink to={'/ngo'} className="mr-14 text-blue-500 font-bold text-lg hover:text-blue-600">NGO</NavLink>
-        <NavLink to={'/restro'} className="mr-14 text-blue-500 font-bold text-lg hover:text-blue-600" >Restaurant</NavLink>
+        <button onClick={() => setSigninNOpen(true)} className="mr-14 text-blue-500 font-bold text-lg hover:text-blue-600">NGO</button>
+        {SigninNOpen ? <SigninN show={SigninNOpen} close={() => setSigninNOpen(false)} /> : <></>}
+       
+        <button onClick={() => setSigninROpen(true)} className="mr-14 text-blue-500 font-bold text-lg hover:text-blue-600" >Restaurant</button>
+        {SigninROpen ? <SigninR show={SigninROpen} close={() => setSigninROpen(false)} /> : <></>}
+
         <NavLink to={'/about'} className="mr-16 text-blue-500 font-bold text-lg hover:text-blue-600">About</NavLink>
       </div>
     </div> 
