b212e3d9438450d55fa901170459c0614be5ff45
Kunal Kishor <kunalbth2002@gmail.com>
2024-01-13 22:27:55 +0530

    home complete

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className="container mt-20 justify-around flex">
+      <div className='about-content ml-40'>
+        About
+      </div>
+
+      <div className="container-pic">
+        <div className="pic2 h-64 bg-green-500 w-64 rounded-[50%] ml-[60%]">
+          <img src="../images//hungry.jpeg" alt="child" className='h-64 w-64 rounded-[50%] ml-2'/>
+        </div>
+      </div>
     </div>
   )
 }
