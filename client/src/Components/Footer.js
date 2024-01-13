215a445c7c0d06405f22ccbe821c54a50ca0289e
Kunal Kishor <kunalbth2002@gmail.com>
2024-01-13 19:16:59 +0530

    home complete

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='mt-36 bg-slate-100 h-36 w-full'>
+    <div className='mt-52 bg-slate-100 h-36 w-full'>
         Footer
     </div>
   )
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
 <div className='home-container flex justify-around mt-20'>
     <div className="home-content">
         <h1 className='text-[#003f88] font-bold text-4xl mt-20 ml-20'>Feed the Hungry, <br /> Spread Joy.</h1>
-        <p className='text-gray-700 ml-20 font-semibold mb-10'>Empower restaurants to bring smiles by donating excess <br />food through our platform.</p>
+        <p className='text-gray-700 ml-20 font-semibold mb-12 mt-4'>Empower restaurants to bring smiles by donating excess <br />food through our platform.</p>
         <NavLink className='bg-green-600 text-white px-6 py-2 rounded-lg font-semibold ml-20 hover:bg-green-500' to={'/restro'} >Donate</NavLink>
         {/* <i class="fa-brands fa-whatsapp text-green-600 text-4xl ml-6 mt-10"></i> */}
     </div>
