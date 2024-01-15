e8de554bff3e3d2bb742a0f882cd591894c3d655
Kunal Kishor <kunalbth2002@gmail.com>
2024-01-15 15:44:25 +0530

    not singup

--- server/index.js
+++ server/index.js
@@ -1,24 +1,34 @@
-const connectToMongo=require('./DB')
-connectToMongo();
-
-
-
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env'});
 const express = require('express')
-// var cors = require('cors')
 const app = express()
-const port = 5000
-// app.use(cors())
-app.use(express.json())
+var cors = require('cors')
+const uploadImage = require('./routes/UploadImg');
+const connectToMongo=require('./DB')
 
 
+app.use(cors())
+app.use(express.json({ limit: "25mb" }));
 
 app.use('/api/auth/ngo',require('./routes/authN'))
 app.use('/api/auth/res',require('./routes/authR'))
 
+connectToMongo();
+const port = process.env.PORT
 
+//code to convert images into url by using the cloudinary version 2 
+app.post("/uploadImage", (req, res) => {
+  uploadImage(req.body.image)
+    .then((url) => res.send(url)) 
+    .catch((err) => res.status(500).send(err));
+});
 
-
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
 app.listen(port, () => {
-  console.log(`iNoteBook app listening on port ${port}`)
+  console.log(`App listening `)
 })
