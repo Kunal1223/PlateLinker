e1389b3784092a72c7f6fa7d96cc800de60a9c4e
Kunal Kishor <kunalbth2002@gmail.com>
2024-01-24 13:04:08 +0530

    adding the packets no

--- client/src/Components/NGO/CardR.js
+++ client/src/Components/NGO/CardR.js
@@ -28,15 +28,18 @@ export default function CardR(props) {
 
             <span className="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Non-Veg</span>
           </div>
-          <div className=" py-4 flex justify-between">
+          <div className=" py-2 flex justify-between">
             <div className="font-bold  "><span className="">Manager: </span>{manager_name}</div>
-
             <div className="">
-
               <a href={social_link}>  <i className="fa-brands fa-instagram mr-2"></i></a>
               <i className="fa-regular fa-envelope mr-2"></i>
               <i className="fa-solid fa-phone "></i>
             </div>
+          </div>
+
+          <div className="packets">
+            <button className="bg-[#dfe3e4] px-2 py-1 rounded-md text-gray-800 font-semibold">Donate Packets  
+            : <span className="ml-4">5 </span><i class="fa-solid fa-circle text-red-500 mr-4 "></i>  4  <i class="fa-solid fa-circle text-green-500 mr-4"></i></button>
           </div>
 
         </div>
